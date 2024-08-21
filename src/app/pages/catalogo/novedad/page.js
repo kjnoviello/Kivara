7875810f95fa845b439a2b86ea68873dafdd5e1b
kevin joel noviello <kjnoviello@gmail.com>
2024-08-21 16:35:00 -0300

    deploy fix

--- src/app/pages/catalogo/novedad/page.js
+++ src/app/pages/catalogo/novedad/page.js
@@ -1,21 +1,21 @@
-import ProductList from '@/app/components/catalogo/ProductList';
-import Header from '@/app/components/header/Header';
+// import ProductList from '@/app/components/catalogo/ProductList';
+// import Header from '@/app/components/header/Header';
 
-export default async function Novedad() {
+// export default async function Novedad() {
 
-    const novedad = await fetch("http://localhost:3000/api/catalogo/novedad")
-        .then(r => r.json())
+//     const novedad = await fetch("http://localhost:3000/api/catalogo/novedad")
+//         .then(r => r.json())
 
-    return (
-        <>
-            <Header />
-            <main className='flex gap-5 flex-wrap p-5 mx-5 items-center justify-center'>
-                {!novedad ? (
-                    <p>loading...</p>
-                ) : (
-                    <ProductList products={novedad} />
-                )}
-            </main>
-        </>
-    )
-}
+//     return (
+//         <>
+//             <Header />
+//             <main className='flex gap-5 flex-wrap p-5 mx-5 items-center justify-center'>
+//                 {!novedad ? (
+//                     <p>loading...</p>
+//                 ) : (
+//                     <ProductList products={novedad} />
+//                 )}
+//             </main>
+//         </>
+//     )
+// }
