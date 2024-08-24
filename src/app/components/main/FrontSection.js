3d588b8b9b10eb816c1cb05d643bf30b2368f78b
kevin joel noviello <kjnoviello@gmail.com>
2024-08-24 15:14:28 -0300

    add animation to main title

--- src/app/components/main/FrontSection.js
+++ src/app/components/main/FrontSection.js
@@ -41,7 +41,7 @@ const FrontSection = () => {
     return (
         <section className="bg-center bg-fixed bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
             <div className="px-4 mx-auto max-w-screen-lg text-center py-24 lg:py-48">
-                <h1 className="mb-4 text-xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl typeAnimation">Potenciamos el futuro del mundo.</h1>
+                <h1 className="inline-block px-2 mb-4 text-xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl typeAnimation">Potenciamos el futuro del mundo.</h1>
                 <p className="mb-8 text-lg font-normal text-gray-300 lg:text-2xl sm:px-16 lg:px-48">Descubre el <strong className='text-white'>celular</strong> perfecto para tu estilo de vida entre una amplia gama de opciones.</p>
                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
 
