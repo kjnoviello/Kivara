324d547b68db92e9050ea6963835a4cca2b78ab2
kevin joel noviello <kjnoviello@gmail.com>
2024-09-14 16:50:36 -0300

    minor fixes

--- src/app/components/shared/navbar.js
+++ src/app/components/shared/navbar.js
@@ -160,30 +160,33 @@ const Navbar = () => {
 
                                     </div>
                                 </div>
-                                <MenuItems
-                                    transition
-                                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                                >
-                                    <MenuItem>
-                                        <Link href={"/pages/carrito"} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 hover:text-indigo-600">
-                                            <span>Ver mi carrito</span>
-                                            <p><strong><i>${valueCart()} - {quantityCart()}u.</i></strong></p>
-                                        </Link>
-                                    </MenuItem>
-                                    <hr />
-                                    <>
-                                        {
-                                            valueQuantityCart > 0 ?
-                                                <MenuItem>
-                                                    <div>
-                                                        <ButtonEmpty inNavbar="ok" />
-                                                    </div>
-                                                </MenuItem>
-                                                :
-                                                ""
-                                        }
-                                    </>
-                                </MenuItems>
+                                <DisclosurePanel>
+                                    <MenuItems
+                                        transition
+                                        className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                                    >
+                                        <MenuItem>
+                                            <Link href={"/pages/carrito"} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 hover:text-indigo-600">
+                                                <span>Ver mi carrito</span>
+                                                <p><strong><i>${valueCart()} - {quantityCart()}u.</i></strong></p>
+                                            </Link>
+                                        </MenuItem>
+                                        <hr />
+                                        <>
+                                            {
+                                                valueQuantityCart > 0 ?
+                                                    <MenuItem>
+                                                        <div>
+                                                            <ButtonEmpty inNavbar="ok" />
+                                                        </div>
+                                                    </MenuItem>
+                                                    :
+                                                    ""
+                                            }
+                                        </>
+                                    </MenuItems>
+                                </DisclosurePanel>
+
                             </Menu>
 
                             {/* Profile dropdown */}
@@ -207,49 +210,50 @@ const Navbar = () => {
                                                 :
                                                 <FaRegUser aria-hidden="true" className=" rounded-full h-6 w-6" />
                                         }
-
                                     </MenuButton>
                                 </div>
 
                                 {/* Si hay usuario se muestra el menu desplegable */}
-                                {
-                                    user.logged ?
-                                        <MenuItems
-                                            transition
-                                            className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                                        >
-                                            <MenuItem>
-                                                <button href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
-                                                    Mi perfil
-                                                </button>
-                                            </MenuItem>
-                                            <MenuItem>
-                                                <button href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
-                                                    Configuración
-                                                </button>
-                                            </MenuItem>
-                                            <MenuItem>
-                                                <button
-                                                    onClick={() => { logoutUser() }}
-                                                    className="block px-4 py-2 text-sm text-[#e02424] data-[focus]:bg-gray-100">
-                                                    <strong>
-                                                        Cerrar sesión
-                                                    </strong>
-                                                </button>
-                                            </MenuItem>
-                                        </MenuItems>
-                                        :
-                                        <MenuItems
-                                            transition
-                                            className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                                        >
-                                            <MenuItem>
-                                                <Link href="/pages/admin" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
-                                                    Iniciar Sesión
-                                                </Link>
-                                            </MenuItem>
-                                        </MenuItems>
-                                }
+                                <DisclosurePanel>
+                                    {
+                                        user.logged ?
+                                            <MenuItems
+                                                transition
+                                                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                                            >
+                                                <MenuItem>
+                                                    <button href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                                                        Mi perfil
+                                                    </button>
+                                                </MenuItem>
+                                                <MenuItem>
+                                                    <button href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                                                        Configuración
+                                                    </button>
+                                                </MenuItem>
+                                                <MenuItem>
+                                                    <button
+                                                        onClick={() => { logoutUser() }}
+                                                        className="block px-4 py-2 text-sm text-[#e02424] data-[focus]:bg-gray-100">
+                                                        <strong>
+                                                            Cerrar sesión
+                                                        </strong>
+                                                    </button>
+                                                </MenuItem>
+                                            </MenuItems>
+                                            :
+                                            <MenuItems
+                                                transition
+                                                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                                            >
+                                                <MenuItem>
+                                                    <Link href="/pages/admin" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                                                        Iniciar Sesión
+                                                    </Link>
+                                                </MenuItem>
+                                            </MenuItems>
+                                    }
+                                </DisclosurePanel>
 
                             </Menu>
                         </div>
@@ -272,16 +276,21 @@ const Navbar = () => {
                         ))}
 
                         {/* Si hay un usuario muestra el boton de Admin */}
-                        <Link href="/pages/admin" key="4">
-                            <DisclosureButton
-                                className={classNames(
-                                    pathname === "/pages/admin" ? 'bg-gray-700 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                    'block rounded-md px-3 py-2 text-base font-medium',
-                                )}
-                            >
-                                Admin
-                            </DisclosureButton>
-                        </Link>
+                        {
+                            user.logged ?
+                                <Link href="/pages/admin" key="4">
+                                    <DisclosureButton
+                                        className={classNames(
+                                            pathname === "/pages/admin" ? 'bg-gray-700 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            'block rounded-md px-3 py-2 text-base font-medium',
+                                        )}
+                                    >
+                                        Admin
+                                    </DisclosureButton>
+                                </Link>
+                                :
+                                ""
+                        }
 
                     </div>
                 </DisclosurePanel>
